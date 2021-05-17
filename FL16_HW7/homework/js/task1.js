602448aa5bd73f15aa4e140a49d03be495b49731
Yaroslav Huzii <guzya21@icloud.com>
2021-05-17 20:57:02 +0300

    HW 7

---
+++ FL16_HW7/homework/js/task1.js
@@ -0,0 +1,39 @@
+let getValue = (amount, years, percent, profit = 0, prevProfit = 0, totalAmount = 0) => {
+  amount = +prompt('initial amount of money', '');
+
+  while (amount < 1000 || !Number.isFinite(amount)) {
+    alert('Invalid input data');
+    amount = +prompt('initial amount of money', '');
+  }
+
+  years = +prompt('number of years', '');
+  while (!Number.isInteger(years) || years < 1 || !Number.isFinite(years)) {
+    alert('Invalid input data');
+    years = +prompt('number of years', '');
+  }
+
+  percent = +prompt(' percentage of a year', '');
+
+  while (percent > 100 || !Number.isFinite(years)) {
+    alert('Invalid input data');
+    percent = +prompt(' percentage of a year', '');
+  }
+
+  profit = (amount * percent) / 100;
+  totalAmount = amount + profit;
+
+  for (let i = 0; i < years - 1; i++) {
+    prevProfit = profit;
+
+    profit = prevProfit + (totalAmount * percent) / 100;
+    totalAmount = amount + profit;
+  }
+
+  return alert(`Initial amount: ${amount}
+Number of years: ${years}
+Percentage of year: ${percent}
+  
+Total profit: ${profit.toFixed(2)}
+Total amount: ${totalAmount.toFixed(2)}`);
+};
+getValue();
---
+++ FL16_HW7/homework/js/task2.js
@@ -0,0 +1,60 @@
+if (confirm('Do you want to play a game?')) {
+  let step = 1;
+  let totalPrize = 0;
+  let prizes = [100, 50, 25];
+  let lastNumber = 8;
+  let attempt = 3;
+  let currentPrize = 0;
+
+  do {
+    let random = Math.floor(Math.random() * (lastNumber + 1));
+
+    for (let i = 0; i < attempt; i++) {
+      let number = prompt(
+        `Choose a roulette number from 0 to ${lastNumber}
+Attempt left: ${attempt - i}
+Total prize: ${totalPrize}$
+Possible prize on current attempt: ${prizes[i]}$`,
+        ''
+      );
+
+      if (random == +number) {
+        currentPrize = prizes[i];
+        totalPrize = totalPrize + currentPrize;
+        i = 3;
+
+        if (confirm(`Congratulation, you won! Your prize is: ${currentPrize}$. Do you want to continue?`)) {
+          step++;
+          lastNumber = lastNumber + 4;
+          prizes = prizes.map((prize) => prize * 2);
+          attempt = 3;
+        } else {
+          alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+          if (confirm('Do you want to play again?')) {
+            step = 1;
+            totalPrize = 0;
+            lastNumber = 8;
+            prizes = [100, 50, 25];
+          } else {
+            step = 0;
+          }
+        }
+      } else if (number == null) {
+        i = 3;
+        step = 0;
+      } else if (i == 2) {
+        alert(`Thank you for your participation. Your prize  is: ${totalPrize}$`);
+        if (confirm('Do you want to play again?')) {
+          step = 1;
+          totalPrize = 0;
+          lastNumber = 8;
+          prizes = [100, 50, 25];
+        } else {
+          step = 0;
+        }
+      }
+    }
+  } while (step);
+} else {
+  alert('You did not become a billionaire, but can.');
+}
