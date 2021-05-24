996f8b7df640da4c457bff0721145f0ba251d864
Yaroslav Huzii <guzya21@icloud.com>
2021-05-24 17:21:17 +0300

    changes HW10

--- FL16_HW10/homework/index.js
+++ FL16_HW10/homework/index.js
@@ -1,20 +1,14 @@
 const isEqual = (a, b) => a === b;
-console.log(isEqual(6, 6));
 
 const isBigger = (a, b) => a > b;
-console.log(isBigger(9, 7));
 
 const storeNames = (...names) => Array.from(names);
-
-console.log(storeNames('Tommy Shelby', 'Ragnar Lodbrok', 'Tom Hardy'));
 
 const getDifference = (firstNum, secondNum) => {
   return +firstNum > +secondNum ? firstNum - secondNum : secondNum - firstNum;
 };
 
-console.log('getDifference', getDifference(3, 5));
-
-function negativeCount(numbers) {
+function negativeCount(...numbers) {
   let counter = 0;
 
   numbers.forEach((number) => {
@@ -25,8 +19,6 @@ function negativeCount(numbers) {
 
   return counter;
 }
-
-console.log('negativeCount', negativeCount([4, -3, 22, 9]));
 
 function letterCount(str1, str2) {
   let counter = 0;
@@ -40,15 +32,14 @@ function letterCount(str1, str2) {
   return counter;
 }
 
-console.log('letterCount', letterCount('Marry', 'y'));
-
-function countPoints(scores) {
+function countPoints(...scores) {
   let counter = 0;
 
   scores.forEach((result) => {
     const pointsArr = result.split(':');
     const firstPoints = +pointsArr[0];
     const secondPoints = +pointsArr[1];
+
     if (firstPoints > secondPoints) {
       counter = counter + 3;
     } else if (firstPoints === secondPoints) {
@@ -58,5 +49,3 @@ function countPoints(scores) {
 
   return counter;
 }
-
-console.log('countPoints', countPoints(['100:90', '110:98', '100:100', '95:46', '54:90', '99:44', '90:90', '111:100']));
