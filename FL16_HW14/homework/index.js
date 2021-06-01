844c0ea2af2b37edfbd149101b6dfea268dee27a
Yaroslav Huzii <guzya21@icloud.com>
2021-06-01 15:17:33 +0300

    HW14 changes

--- FL16_HW14/homework/index.js
+++ FL16_HW14/homework/index.js
@@ -11,7 +11,7 @@ table.addEventListener('click', (e) => {
   }
 
   if (e.target === specialCell) {
-    table.className = 'green';
+    table.className = 'yellow';
   }
 });
 
@@ -48,5 +48,60 @@ sendButton.addEventListener('click', showValidMessage);
 /* END TASK 2 */
 
 /* START TASK 3: Your code goes here */
+
+const basketCourt = document.getElementById('basket-court');
+const ball = document.getElementById('ball');
+let countA = 0;
+let countB = 0;
+const teamA = document.getElementById('first-team');
+const teamB = document.getElementById('second-team');
+const scoreMessage = document.getElementById('score-message');
+
+teamA.textContent = 0;
+teamB.textContent = 0;
+
+function getClickPosition(e) {
+  const xPosition = e.clientX;
+  console.log(xPosition);
+  const yPosition = e.clientY;
+  console.log(yPosition);
+  const ballXPos = 760;
+  const ballYPos = e.clientY;
+  const clickX = xPosition - ballXPos;
+  const clickY = yPosition - ballYPos;
+  const translateValue = `translate( ${clickX + 'px'}, ${clickY + 'px'} )`;
+
+  if (xPosition >= '500' && xPosition < '510') {
+    ball.style.transform = translateValue;
+    showMessageA();
+    setTimeout(hideMessage, 3000);
+  } else if (xPosition >= '1015' && xPosition <= '1025') {
+    ball.style.transform = translateValue;
+    showMessageB();
+    setTimeout(hideMessage, 3000);
+
+    ballBack();
+  }
+}
+function ballBack() {
+  ball.style.transform = `translate( ${0 + 'px'}, ${0 + 'px'} )`;
+}
+
+function hideMessage() {
+  scoreMessage.style.display = 'none';
+}
+
+function showMessageA() {
+  teamA.textContent = ++countA;
+  scoreMessage.style.display = 'block';
+  scoreMessage.textContent = 'Team A scored';
+}
+function showMessageB() {
+  teamB.textContent = ++countB;
+  scoreMessage.style.display = 'block';
+  scoreMessage.textContent = 'Team B scored';
+}
+
+basketCourt.addEventListener('click', getClickPosition);
 
 /* END TASK 3 */
