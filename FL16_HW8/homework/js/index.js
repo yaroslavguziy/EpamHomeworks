d8f1f9b38d7fe06eb1d8cc66b5a7a82377f8661d
Yaroslav Huzii <guzya21@icloud.com>
2021-07-14 14:58:38 +0300

    stage2 HW8

--- FL16_HW8/homework/js/index.js
+++ FL16_HW8/homework/js/index.js
@@ -1,3 +1,50 @@
-// Write your code here
+$(document).ready(function () {
+  $('.calc').click(function () {
+    $('#display').val($('#display').val() + $(this).val());
+  });
+  $('.clear').click(function () {
+    $('#display').attr('placeholder', '0').val('').css('color', 'black');
+  });
+  $('.result').click(function () {
+    const result = function (fn) {
+      return new Function('return ' + fn)();
+    };
 
-// $(".window").text("ERROR").css("color","red")
+    const currentValue = $('#display').val();
+
+    if (currentValue.includes('/0')) {
+      $('#display').val('ERROR').css('color', 'red');
+    } else {
+      const log = $('<div/>').addClass('log');
+      const circle = $('<div/>').addClass('circle');
+      const info = $('<div/>')
+        .addClass('info')
+        .text(currentValue + '=' + result(currentValue));
+      if (info.text().includes('48')) {
+        info.css('text-decoration', 'underline');
+      } else {
+        info.css('text-decoration', 'none');
+      }
+      const deleteLog = $('<button/>').addClass('delete').text('x');
+      log.append(circle);
+      log.append(info);
+      log.append(deleteLog);
+      $('#logs').prepend(log);
+
+      $('#display').val(result(currentValue));
+    }
+
+    const logs = $('.calculator-logs');
+    if (logs.scroll()) {
+      console.log(logs.scrollTop());
+    }
+
+    $('.circle').click(function () {
+      $(this).toggleClass('red');
+    });
+
+    $('.delete').click(function () {
+      $(this).closest('div').remove();
+    });
+  });
+});
