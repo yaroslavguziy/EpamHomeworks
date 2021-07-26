c2c1140e2de342a4f25fbc2f65bbc157e9db6d92
Yaroslav Huzii <guzya21@icloud.com>
2021-07-26 13:42:56 +0300

    HW 12  changes

--- FL16_HW12/homework/spec/birthday.service.spec.js
+++ FL16_HW12/homework/spec/birthday.service.spec.js
@@ -1,10 +1,22 @@
-class BirthdayService {
-  howLongToMyBirthday(date) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        reject;
-        return resolve;
-      }, 100);
-    });
-  }
-}
+const getBigestNumber = require('../src/get-bigest-number');
+
+describe('getBigestNumber', () => {
+  it('should return bigest number', () => {
+    expect(getBigestNumber(1, 2, 3, 4)).toBe(4);
+  });
+
+  it('throw Error if arguments length < 2', () => {
+    const result = getBigestNumber(1);
+    expect(() => result).toThrow(new Error('Not enough arguments'));
+  });
+
+  it('throw Error if arguments length > 10', () => {
+    const result = getBigestNumber(1, 2, 3, 4, 5, 6, 7, 13, 21, 12, 321);
+    expect(() => result).toThrow(new Error('Too many arguments'));
+  });
+
+  it('throw Error if type of any argument not a number', () => {
+    const result = getBigestNumber(1, 2, 3, '5');
+    expect(() => result).toThrow(new Error('Wrong argument type'));
+  });
+});
--- FL16_HW12/homework/src/birthday.service.js
+++ FL16_HW12/homework/src/birthday.service.js
@@ -1,1 +1,17 @@
-// Your code goes here
+function getBigestNumber(...arguments) {
+  arguments.forEach( (arg) => {
+    if (typeof arg !== 'number') {
+      throw Error('Wrong argument type') ;
+    }
+  })
+
+  if (arguments.length > 10) {
+    throw Error('Too many arguments') ;
+  } else if (arguments.length < 2) {
+    throw Error('Not enough arguments') ;
+  }
+
+  return Math.max(...arguments)
+} 
+
+module.exports = getBigestNumber;
